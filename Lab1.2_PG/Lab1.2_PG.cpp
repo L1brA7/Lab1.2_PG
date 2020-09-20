7946f7c91238f93e1e84e75c67051df183c87602
L1brA7 <umashev58@gmail.com>
2020-09-20 20:35:54 +0300

    first task done

--- Lab1.2_PG/Lab1.2_PG.cpp
+++ Lab1.2_PG/Lab1.2_PG.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <regex>
+#include <tuple>
 
 using namespace std;
 
@@ -9,19 +10,27 @@ struct DATE {
     int day;
     int month;
     int year;
+    bool leap = false;
     void construct(string dateline) {
         string day_n, month_n, year_n;
-        day_n = dateline[0] + dateline[1];
-        month_n = dateline[3] + dateline[4];
         int i = 6;
         while(dateline[i]) {
             year_n += dateline[i];
             i++;
         }
-
-        day = stoi(day_n);
-        month = stoi(month_n);
+        day = (dateline[0] - '0') * 10 + dateline[1] - '0';
+        month = (dateline[3] - '0') * 10 + dateline[4] - '0';
         year = stoi(year_n);
+        if (year % 100 == 0) {
+            if (year % 400 == 0) leap = true;
+        }
+        else if (year % 4 == 0) leap = true;
+
+        if (month == 2) {
+            if (leap == false && day == 29) {
+                day = 0;
+            }
+        }
     }
     void output() {
         string DATEline = "";
@@ -40,59 +49,95 @@ struct DATE {
             zeroes--;
         }
     }  
-    DATEline += to_string(year);
+    DATEline += to_string(year) + ' ';
 
-    printf("%s\n", DATEline.c_str());
+    printf("%s", DATEline.c_str());
     }
-    void go_next(DATE *ARR, int i) {
-        if(i != sizeof(ARR) / sizeof(ARR[0]) - 1) {
-            i++;
-            day = ARR[i].day;
-            month = ARR[i].month;
-            year = ARR[i].year;
+    void next_date(DATE *D_ARR, int i, int N) {
+        if(i < N - 1) {
+            D_ARR[i+1].output();
         }
         else {
-            printf("Its the last one in list");
+            printf(" LAST ONE");
         }
     }
-    void go_back(DATE *ARR, int i) {
+    void prev_date(DATE *D_ARR, int i) {
         if(i != 0) {
-            i--;
-            day = ARR[i].day;
-            month = ARR[i].month;
-            year = ARR[i].year;
+            D_ARR[i-1].output();
         }
         else {
-            printf("Its the first one on list");
+            printf(" FIRST ONE ");
         }
     }
 };
 
-DATE *array_construct();
+tuple<DATE*, int> array_construct();
+tuple<DATE*, int> array_filter(DATE* NF_D_ARR, int N);
+
 
 int main() {
-    DATE *D_ARR = array_construct();
-
+    DATE *D_ARR;
+    int N;
+    tie(D_ARR, N) = array_construct();
+    
+    for(int i = 0; i < N; i++) {
+        printf("%i) ", i + 1);
+        D_ARR[i].output();
+        D_ARR[i].prev_date(D_ARR, i);
+        D_ARR[i].next_date(D_ARR, i, N);
+        printf("\n");
+    }
     return 0;
 }
 
-DATE *array_construct() {
+tuple<DATE*, int> array_construct() {
     ifstream DATES;
-    DATES.open("dates.txt");
     string dateline;
-    regex DATE_REGEX("(([0-2][1-9]|30)[-./\\](0[469]|11)[-./\\]([0-9]*)) || (([0-2][1-9]|3[0-1])[-./\\](0[1358]|1[02])[-./\\]([0-9]*)) || (([0-2][1-9])[-./\\]02[-./\\]([0-9]*))");
-    int N = 0, i = 0;
-    while(getline(DATES, dateline)) N++;
-    DATES.seekg(0);
-    DATE *D_ARR = new DATE[N];
-    DATE *non_filtered_D_ARR = new DATE[N];
+    regex DATE_REGEX("(([0-2][1-9]|30)[-./](0[469]|11)[-./]([0-9]*))|(([0-2][1-9]|3[0-1])[-./](0[1358]|1[02])[-./]([0-9]*))|(([0-2][1-9])[-./]02[-./]([0-9]*))");
+    int N = 0, i = 0;   
+    
+    DATES.open("dates.txt");
     while(getline(DATES, dateline)) {
         if (regex_match(dateline, DATE_REGEX)) {
-            D_ARR[i].construct(dateline);
+            N++;
+        }
+    }
+    DATE *D_ARR = new DATE[N];
+    DATE *NF_D_ARR = new DATE[N];
+
+    DATES.close();
+    DATES.open("dates.txt");
+
+    while(getline(DATES, dateline)) {
+        
+        if (regex_match(dateline, DATE_REGEX)) {
+            NF_D_ARR[i].construct(dateline);
             i++;
         }
     }
-
     DATES.close();
-    return D_ARR;
+    tie(D_ARR, N) = array_filter(NF_D_ARR, N);
+    return make_tuple(D_ARR, N);
+}
+
+tuple<DATE*, int> array_filter(DATE *NF_D_ARR, int N) {
+    int newN = 0;
+    for (int i = 0; i < N; i++) {
+        if (NF_D_ARR[i].day != 0) {
+            newN++;
+        }
+    }
+    
+    DATE* D_ARR = new DATE[newN];
+    int k = 0;
+    for (int i = 0; i < N; i++) {
+        if (NF_D_ARR[i].day != 0) {
+            D_ARR[k].day = NF_D_ARR[i].day;
+            D_ARR[k].month = NF_D_ARR[i].month;
+            D_ARR[k].year = NF_D_ARR[i].year;
+            D_ARR[k].leap = NF_D_ARR[i].leap;
+            k++;
+        }
+    }
+    return make_tuple(D_ARR, newN);
 }
